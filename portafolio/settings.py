58b1b72dfdffb67ac3957d41d22c027f4d23f2f6
javicente03 <javicentego@gmail.com>
2022-04-15 17:22:50 -0400

    gunicorns

--- portafolio/settings.py
+++ portafolio/settings.py
@@ -15,7 +15,7 @@ environ.Env.read_env()
 SECRET_KEY = 'django-insecure-&z(nfj@s2#iq$@+nb(c-!gs_m101-$x8v14an&^7_gr&0o09%8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 # EMAIL
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
