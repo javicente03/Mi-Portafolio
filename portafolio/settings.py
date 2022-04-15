9b056b672cf9a1d5f80fc776ea281afa0b2573a5
javicente03 <javicentego@gmail.com>
2022-04-15 17:20:42 -0400

    gunicorn

--- portafolio/settings.py
+++ portafolio/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
-    # 'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'portafolio.urls'
@@ -124,17 +124,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT=  BASE_DIR / 'static_files/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# STATIC_URL = '/static/'
+# STATIC_URL = 'static/'
+# STATIC_ROOT=  BASE_DIR / 'static_files/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
 
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static_files'),
-# )
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static_files'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
--- portafolio/urls.py
+++ portafolio/urls.py
@@ -2,13 +2,12 @@ from django.urls.conf import include
 from django.contrib import admin
 from django.urls import path
 from welcome.views import Welcome
-# from django.conf import settings
-# from django.conf.urls.static import static
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
     path('', Welcome.as_view(), name='welcome'),
     path('welcome/', include('welcome.urls')),
-] 
-# + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
